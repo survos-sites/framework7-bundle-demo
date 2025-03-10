3c3eb81547b05c66a4da8d02e854e8af4026cc19
Tac Tacelosky <tacman@gmail.com>
2025-03-10 11:51:28 -0400

    play with pages, etc.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,19 +2,86 @@
 
 namespace App\Controller;
 
+use Knp\Menu\FactoryInterface;
+use Survos\FwBundle\Event\KnpMenuEvent;
+use Symfony\Bridge\Twig\Attribute\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AppController extends AbstractController
 {
-    #[Route('/', name: 'app_homepage')]
+    public function __construct(
+        private EventDispatcherInterface $eventDispatcher,
+        protected FactoryInterface $factory,
+    )
+    {
+
+    }
+    #[Route('/basic', name: 'app_basic')]
+    #[Template('examples/basic.html.twig')]
+    public function basic(): Response|array
+    {
+        return [];
+    }
+
+    #[Route('/home', name: 'app_homepage')]
     public function homepage(): Response
     {
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
         ]);
     }
+
+    #[Route('/about', name: 'app_about')]
+    #[Template('app/about.html.twig')]
+    public function about(): Response|array
+    {
+        return [];
+    }
+
+    #[Route('/', name: 'app_index', options: ['expose' => true], methods: ['GET'])]
+    public function mobile(Request $request): Response
+    {
+
+        // iterate through the page and tab routes to create templates, which will be rendered in the main page.
+        $menu = $this->factory->createItem($options['name'] ?? KnpMenuEvent::class);
+        foreach ([KnpMenuEvent::MOBILE_TAB_MENU  => 'tab',
+                     KnpMenuEvent::MOBILE_PAGE_MENU => 'page',
+                     KnpMenuEvent::MOBILE_UNLINKED_MENU => 'page',
+                 ] as $eventName=>$type) {
+            $options = [];
+            $options = (new OptionsResolver())
+                ->setDefaults([
+
+                ])
+                ->resolve($options);
+            $this->eventDispatcher->dispatch(new KnpMenuEvent($menu, $this->factory, $options), $eventName);
+            foreach ($menu->getChildren() as $route=>$child) {
+                try {
+                    $template = "app/$route.html.twig";
+                    $params = [
+                        'type' => $type,
+                        'route' => $route,
+                        'template' => $template,
+                        'debug' => $request->get('debug', false),
+                    ];
+//                    $templates[$route]  = $this->twig->render($template, $params);
+                    $templates[$route] = $this->renderView($template, $params);
+                } catch (\Exception $e) {
+                    dd($route, $template, $e->getMessage(), $e);
+                }
+            }
+        }
+        return $this->render('@SurvosFw/start.html.twig', [
+            'templates' => $templates,
+            'playNow' => $request->get('playNow', true),
+        ]);
+    }
+
 
 //    #[Route('/pages/settings.html', name: 'app_settings', priority: 500)]
 //    public function settings(): Response
