0bb44955c44fee5b7f9a847745ffac265e6a74d8
Tac Tacelosky <tacman@gmail.com>
2025-03-26 12:48:13 -0400

    panther tests with screenshots

---
+++ tests/PantherTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Component\Panther\PantherTestCase;
+use Zenstruck\Browser\Test\HasBrowser;
+
+class PantherTest extends PantherTestCase
+{
+    use HasBrowser;
+
+//    public function testSomething(): void
+//    {
+//        $client = static::createPantherClient();
+//        $crawler = $client->request('GET', '/');
+//        $this->assertSelectorTextContains('h1', 'Hello World');
+//    }
+
+    public function testDemo(): void
+    {
+        // the home page that browses projects (not fw7)
+        $browser = $this->pantherBrowser()
+            ->visit('/')
+            ->assertOn('/')
+            ->takeScreenshot('home.png');
+
+        $browser
+            ->visit('/en/batsi#tab-locations')
+            ->takeScreenshot('basti-locations.png')
+        ;
+
+        $browser
+//            ->waitUntilSeeIn('body', '#tab-artists')
+            ->click('#tab-artists') // click on the artists
+            ->takeScreenshot('artists.png')
+            ;
+
+        $browser->click('Artwork')
+            ->wait(200) // @todo: wait for the tab 'obras' to be visible in the dom, or the tab to be marked as selected.
+            ->takeScreenshot('artwork.png')
+        ;
+
+
+    }
+
+
+}
